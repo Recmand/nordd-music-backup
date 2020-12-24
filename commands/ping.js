31325059cd344bf7f1353f8c999aaf4bd9853e97
Glitch (faceted-excited-jester) <none>
2020-12-24 04:57:08 +0000

    🏖🐁 Checkpoint ./commands/ping.js:25557114/1

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "ping",
-  cooldown: 10,
+  cooldown: 0,
   description: "Show the bot's average ping",
   run: async(client, message) => {
     message.reply(`📈 Average ping to API: ${Math.round(message.client.ws.ping)} ms`).catch(console.error);
